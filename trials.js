96d40abaeeef40175f3a8d55ba8ac610d3b3d5ba
Hackbright Student <no-reply@hackbrightacademy.com>
2019-05-01 17:45:54 -0700

    finished printing highly complex businessLoan

--- trials.js
+++ trials.js
@@ -59,6 +59,8 @@ function addTransaction(date, amount) {
   transactions.set(date, amount); 
 }
 
+// Use the function to add transactions
+
 addTransaction('May-2', -500)
 addTransaction('May-13', 1200)
 addTransaction('May-15', -100)
@@ -66,7 +68,7 @@ addTransaction('May-21', -359)
 addTransaction('May-29', 2200)
 
 
-// Use the function to add transactions
+// Add function to show balance status
 
 // Takes in one parameter. 
 // For looop so taht if the acc balance is 0
@@ -85,30 +87,100 @@ function showBalanceStatus(balance){
     console.log("Thank you for your business");
   }
 }
-// Add function to show balance status
 
 
 // Add function to show transactions
 
+function showTransactions(map, beginningBalance){
+
+  console.log(`Begining Balane: ${beginningBalance}`);
+
+  // Get each transaction from the Map and displa the date(key[0])
+  // Display the type (- / +) by determinging if pos/neg.
+  // Display the amount of changed.
+  // Display new total balance.
+
+  let endingbalance = beginningBalance
+
+  for (let [date, value] of transactions) {  
+    if (value > 0) {
+      var transactionType = 'Deposit';
+    }
+    else {
+      var transactionType = 'Withdrawal';
+    }
+
+    endingbalance += value;
+
+    if (endingbalance < 0) {
+      endingbalance -= 25;
+    }
+
+    console.log(`Date: ${date}, Type: ${transactionType}, Amount Change: $${value}`);
+    console.log(`Ending Balance: $${endingbalance}`);
+  }
+}
+
+console.log(showTransactions(transactions, 10))
 
 // ///////////////////////////////////////////////////////
 // All Customer Info:
 
 // Make an object with customer info
-
+let customerInfo = {
+    'Account Holder': accountHolder,
+    'Account Number': accountNumber,
+    'Business Name': businessName,
+    'Addresses': addresses,
+    'Phone Numbers': phoneNumbers,
+    'Transactions': transactions,
+    'Starting Balance': 2600
+}
 
 // Function to add customer attributes
 
+function addCustomerAttributes(favMelon, pets=0) {
+  customerInfo['Favorite Melon'] = favMelon;
+  customerInfo['Pet'] = pets;
+}
+
 
 // Add attributes for this user
-
+addCustomerAttributes('Casabas', 6);
+console.log(customerInfo);
 
 // ///////////////////////////////////////////////////////
 // Getting a Business Loan
+function businessLoan(annualIncome, customerInfo) {
+  
+  //if annual income < 100,000 AND customerInfo[favorite Melon] === Melon || pets > 5
+    //CONSOLE.LOG(5%)
+    //ELSE 
+      //CONSOLE.LOG(8%)
+  
+  if (annualIncome < 100000) {
+    if (customerInfo['Favorite Melon'] === 'Casaba' || customerInfo['Pet'] > 5) {
+    console.log('5%');
+    } else {
+    console.log('8%');
+    }
+  }
 
+  if (annualIncome > 100000 && annualIncome < 200000) {
+    if (customerInfo['Favorite Melon'] === 'Casaba' || customerInfo['Pet'] > 5) {
+      console.log('4%');
+    } else {
+      console.log('7%');
+    }
+  }
+  
+  if (annualIncome > 200000) {
+    console.log('4%')
+  }
+}
 
 // Function to return loan rate
-
+businessLoan(150000, customerInfo)
 
 // ///////////////////////////////////////////////////////
 // Account Report
